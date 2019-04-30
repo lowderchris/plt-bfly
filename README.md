89cc70d7879cca42f85187f122d91073b75a5e28
Chris Lowder <lowder.chris@icloud.com>
2019-04-30 09:17:51 -0600

    First completed version of the script, only smaller refinements from here

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # plt-bfly
 Generate a solar magnetic field butterfly diagram
 
+## Grabbing data
+HMI data can be acquired using JSOC, or other similar tools.
+
+To access the set of polar corrected MDI synoptic charts...
+    curl -O http://soi.stanford.edu/magnetic/synoptic/carrot/M_Corr/synop_Mr_0.polfil.[1911-2104].fits
+
 ## To-do
-- Refine plot formatting a bit
-- Remove hardcoded bits
-- Incorporate SOHO/MDI data
+- Add traditional date axis
 - Automate data query (https://docs.sunpy.org/en/stable/guide/acquiring_data/jsoc.html)
 - ???
--- plt-bfly.py
+++ plt-bfly.py
@@ -3,20 +3,59 @@
 # Import libraries
 import sunpy
 import sunpy.io
+import os.path
 
 # Specify data directories and data range
-hmidat = '/Users/clowder/data/hmi.Synoptic_MR_720s/'
-crs = np.arange(2097, 2213)
+mdidat = '/Users/clowder/data/mdi.Synoptic_Mr.polfil/'
+mdicrs = np.arange(1911, 2096)
+hmidat = '/Users/clowder/data/hmi.Synoptic_Mr.polfil/'
+hmicrs = np.arange(2097, 2215)
+
+# Specify any parameters needed for the output map
+latres = 1440
+lat = np.linspace(-1,1,latres)
 
 # Generate any output arrays
-bfly = np.zeros([1440, len(crs)], dtype=np.double)
+bfly = np.zeros([latres, len(mdicrs)+len(hmicrs)], dtype=np.double)
 i = 0
 
-# Loop
-for cr in crs:
-
+# Loop through MDI files
+for cr in mdicrs:
     # Read and process a given synoptic chart into a profile
-    d = sunpy.io.read_file(hmidat+'hmi.Synoptic_Mr_720s.'+str(cr).zfill(4)+'.synopMr.fits')
-    crdat = d[0].data
-    bfly[:,i] = crdat.sum(axis=1)/3600
+    filenm = mdidat+'synop_Mr_0.polfil.'+str(cr).zfill(4)+'.fits'
+    if os.path.isfile(filenm):
+        d = sunpy.io.read_file(filenm)
+        crdat = d[0].data
+        crdat[where(abs(crdat) > 3000)] = 0
+        bfly[:,i] = np.interp(lat,np.linspace(-1,1,crdat.shape[0]),crdat.sum(axis=1)/sum(isfinite(crdat[540,:])))
+    else:
+        bfly[:,i] = np.NaN
     i += 1
+
+# Loop through HMI files
+for cr in hmicrs:
+    # Read and process a given synoptic chart into a profile
+    filenm = hmidat+'hmi.synoptic_mr_polfil_720s.'+str(cr).zfill(4)+'.Mr_polfil.fits'
+    if os.path.isfile(filenm):
+        d = sunpy.io.read_file(filenm)
+        crdat = d[1].data
+        bfly[:,i] = crdat.sum(axis=1)/crdat.shape[1]
+    else:
+        bfly[:,i] = np.NaN
+    i += 1
+
+# Plot the resulting diagram
+f, (ax1) = subplots(1, figsize=[8,3])
+im = ax1.imshow(bfly, vmin=-10, vmax=10, extent=[mdicrs[0],hmicrs[-1],-1,1], aspect='auto')
+ax1.set_yticks([-1,-0.5,0,0.5,1])
+ax2 = ax1.twinx()
+latticks = np.array([-90,-60,-30,30,60,90])
+ax2.set_yticks(sin(latticks*np.pi/180))
+ax2.set_yticklabels(latticks)
+ax1.set_xlabel('Carrington rotation')
+ax1.set_ylabel('Sine latitude')
+ax2.set_ylabel('Latitude (degrees)')
+cb = colorbar(im, ax=ax2, label='Mean magnetic field (G)', use_gridspec=True, fraction=0.05, pad=0.1, extend='both')
+tight_layout()
+savefig('bfly.pdf')
+savefig('bfly.png')

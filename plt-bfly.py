84be2d94f0a001bf00cf80b765137b083781e6e8
Chris Lowder <lowder.chris@icloud.com>
2019-08-12 19:18:43 -0600

    I love working with datetime strings.

--- plt-bfly.py
+++ plt-bfly.py
@@ -4,19 +4,25 @@
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import sunpy
 import sunpy.io
 import os.path
 
 # Specify data directories and data range
+cr0 = 1911
+cr1 = 2219
 mdidat = os.path.expanduser('~/data/mdi.Synoptic_Mr.polfil/')
-mdicrs = np.arange(1911, 2096)
+mdicrs = np.arange(cr0, 2096)
 hmidat = os.path.expanduser('~/data/hmi.Synoptic_Mr.polfil/')
-hmicrs = np.arange(2097, 2215)
+hmicrs = np.arange(2097, cr1)
 
 # Specify any parameters needed for the output map
 latres = 1440
 lat = np.linspace(-1,1,latres)
+
+# Generate a time array
+tarr = []
 
 # Generate any output arrays
 bfly = np.zeros([latres, len(mdicrs)+len(hmicrs)], dtype=np.double)
@@ -28,6 +34,7 @@ for cr in mdicrs:
     filenm = mdidat+'synop_Mr_0.polfil.'+str(cr).zfill(4)+'.fits'
     if os.path.isfile(filenm):
         d = sunpy.io.read_file(filenm)
+        tarr.append(d[0].header['T_ROT'])
         crdat = d[0].data
         crdat[np.where(abs(crdat) > 3000)] = 0
         bfly[:,i] = np.interp(lat,np.linspace(-1,1,crdat.shape[0]),crdat.sum(axis=1)/sum(np.isfinite(crdat[540,:])))
@@ -41,24 +48,33 @@ for cr in hmicrs:
     filenm = hmidat+'hmi.synoptic_mr_polfil_720s.'+str(cr).zfill(4)+'.Mr_polfil.fits'
     if os.path.isfile(filenm):
         d = sunpy.io.read_file(filenm)
+        tarr.append(d[1].header['T_ROT'])
         crdat = d[1].data
         bfly[:,i] = crdat.sum(axis=1)/crdat.shape[1]
     else:
         bfly[:,i] = np.NaN
     i += 1
 
+# Convert the timing array
+tarr_cr = [ datetime.datetime.strptime((tarr[x])[0:16], "%Y.%m.%d_%H:%M") for x in range(len(tarr)) ]
+tmin, tmax = mdates.date2num([tarr_cr[0], tarr_cr[-1]])
+
 # Plot the resulting diagram
-f, (ax1) = plt.subplots(1, figsize=[8,3])
-im = ax1.imshow(bfly, vmin=-10, vmax=10, extent=[mdicrs[0],hmicrs[-1],-1,1], aspect='auto')
+f, (ax1) = plt.subplots(1, figsize=[8,4])
+im = ax1.imshow(bfly, vmin=-10, vmax=10, extent=[tmin,tmax,-1,1], aspect='auto')
 ax1.set_yticks([-1,-0.5,0,0.5,1])
-ax2 = ax1.twinx()
+ax1a = ax1.twinx()
+ax1b = ax1.twiny()
 latticks = np.array([-90,-60,-30,30,60,90])
-ax2.set_yticks(np.sin(latticks*np.pi/180))
-ax2.set_yticklabels(latticks)
-ax1.set_xlabel('Carrington rotation')
+ax1a.set_yticks(np.sin(latticks*np.pi/180))
+ax1a.set_yticklabels(latticks)
+ax1b.set_xlim(cr0, cr1)
+ax1.xaxis_date()
+ax1.set_xlabel('Year')
 ax1.set_ylabel('Sine latitude')
-ax2.set_ylabel('Latitude (degrees)')
-cb = plt.colorbar(im, ax=ax2, label='Mean magnetic field (G)', use_gridspec=True, fraction=0.05, pad=0.1, extend='both')
-f.tight_layout()
+ax1a.set_ylabel('Latitude (degrees)')
+ax1b.set_xlabel('Carrington rotation')
+cb = plt.colorbar(im, ax=ax1a, label='Mean magnetic field (G)', use_gridspec=True, fraction=0.05, pad=0.1, extend='both')
+#f.tight_layout()
 plt.savefig('bfly.pdf', dpi=300)
 plt.savefig('bfly.png', dpi=300)
